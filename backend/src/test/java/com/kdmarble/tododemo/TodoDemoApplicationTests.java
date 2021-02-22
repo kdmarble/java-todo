ca133959b674ee4d54d719513ca9f397610f0bc8
Keith Marble <kmarble96@gmail.com>
2021-02-22 09:51:02 -0500

    init repo

---
+++ backend/src/test/java/com/kdmarble/tododemo/TodoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdmarble.tododemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
